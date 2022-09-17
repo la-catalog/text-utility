8fbf14aeef87b958a5b7ec34d91b21391d39d942
thiagola92 <thiagola92@users.noreply.github.com>
2022-09-17 14:28:21 -0300

    Workflow fix

--- text_utility/gtin.py
+++ text_utility/gtin.py
@@ -1,6 +1,8 @@
 from collections.abc import Iterator
 
 from gtin import get_gcp, has_valid_check_digit
+
+
 def find_gtins(text: str) -> Iterator[str]:
     numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     previous_not_number = ""
